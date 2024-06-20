3c6c65aeb0ffb84ee26f57420778337f4f4ea09d
Adam <adam.wren@ten10.com>
2024-06-20 12:19:00 +0100

    Created game function, round function and scores

--- javascript.js
+++ javascript.js
@@ -1,35 +1,90 @@
-console.log("Rock, Paper, Scissors")
 
-function getComputerChoice(){
-    let computerChoice = Math.round(Math.random() * 100)
-if (computerChoice < 33){
-    return "Rock"
-}
-if (computerChoice >= 33 && computerChoice < 66){
-    return "Paper"
-}
-if (computerChoice >= 66){
-    return "Scissors"
-}
-}
+function playGame(){
 
-function getHumanChoice(){
-    let humanChoice = prompt("Do you want to play a game? Please choose; Rock, Paper or Scissors")
-    if (humanChoice.toUpperCase() === "ROCK"){
-        return ("Rock")
+    let humanScore = "0"
+    let computerScore = "0"
+
+    function getComputerChoice(){
+        let randomNumber = Math.round(Math.random() * 100)
+    if (randomNumber < 33){
+        return "Rock"
     }
-    if (humanChoice.toUpperCase() === "PAPER"){
-        return ("Paper")
-    } 
-    if (humanChoice.toUpperCase() === "SCISSORS"){
-        return ("Scissors")
-    } 
-    else {
-        return "Please enter a valid move"
+    if (randomNumber >= 33 && randomNumber < 66){
+        return "Paper"
     }
+    if (randomNumber >= 66){
+        return "Scissors"
+    }
+    }
+    
+    
+    function getHumanChoice(){
+        let choice = prompt("Do you want to play a game? Please choose; Rock, Paper or Scissors")
+        if (choice.toUpperCase() === "ROCK"){
+            return ("Rock")
+        }
+        if (choice.toUpperCase() === "PAPER"){
+            return ("Paper")
+        } 
+        if (choice.toUpperCase() === "SCISSORS"){
+            return ("Scissors")
+        } 
+        else {
+            return "an invalid move"
+        }
+    }
+    
+    
+    function playRound(humanSelection, computerSelection){
+        console.log("You picked " + humanSelection + ", the computer picked " + computerSelection + "!")
+        if (humanSelection === computerSelection){
+            console.log("Draw! you both picked the same!")
+        }
+        if (humanSelection === "an invalid move"){
+            console.log("Player disqualified, the computer wins this round!")
+            return computerScore++
+        }
+        if(humanSelection === "Rock" && computerSelection === "Scissors"){
+            console.log("You win! Rock beats Scissors!")
+            return humanScore++
+        }
+        if(humanSelection === "Paper" && computerSelection === "Rock"){
+            console.log("You win! Paper beats Rock!")
+            return humanScore++
+        }
+        if(humanSelection === "Scissors" && computerSelection === "Paper"){
+            console.log("You win! Scissors beats Paper!")
+            return humanScore++
+        }
+        if(humanSelection === "Rock" && computerSelection === "Paper"){
+            console.log("You lose! Paper beats Rock!")
+            return computerScore++
+        }
+        if(humanSelection === "Paper" && computerSelection === "Scissors"){
+            console.log("You lose! Scissors beats Paper")
+            return computerScore++
+        }
+        if(humanSelection === "Scissors" && computerSelection === "Rock"){
+            console.log("You lose! Rock beats Scissors!")
+            return computerScore++
+        }
+    }
+
+    let humanChoice = getHumanChoice()
+    let computerChoice = getComputerChoice()
+   
+
+    playRound(humanChoice, computerChoice)
+    console.log(humanScore + " " + computerScore)
+    playRound(humanChoice, computerChoice)
+    console.log(humanScore + " " + computerScore)
+    playRound(humanChoice, computerChoice)
+    console.log(humanScore + " " + computerScore)
+
 }
 
+playGame()
 
-console.log(getHumanChoice())
 
-console.log(getComputerChoice())
+
+
